5932e540b00967698262e890377d969786224467
Rodrigo Lorca <lorchaos@gmail.com>
2011-10-25 14:30:49 +0200

    Initial implementation finished

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -92,4 +92,63 @@ public class BoardTest
         System.out.println(b.toString());
     }
 
+    @Test(timeout = 1000)
+    public void realLifeProblem()
+    {
+        byte[][] init = new byte[][]{           {0, 0, 0, 2, 6, 0, 7, 0, 1},
+                                                {6, 8, 0, 0, 7, 0, 0, 9, 0},
+                                                {1, 9, 0, 0, 0, 4, 5, 0, 0},
+                                                {8, 2, 0, 1, 0, 0, 0, 4, 0},
+                                                {0, 0, 4, 6, 0, 2, 9, 0, 0},
+                                                {0, 5, 0, 0, 0, 3, 0, 2, 8},
+                                                {0, 0, 9, 3, 0, 0, 0, 7, 4},
+                                                {0, 4, 0, 0, 5, 0, 0, 3, 6},
+                                                {7, 0, 3, 0, 1, 8, 0, 0, 0}};
+
+
+
+        Board b = new Board(init);
+
+        while(!b.isSolved())
+        {
+            final Board.Cell next = b.findNextCandidate();
+
+            final byte[] candidates = next.getCandidateValues();
+
+            b = next.setValue(candidates[0]);
+        }
+
+        System.out.println(b.toString());
+    }
+
+
+    @Test(timeout = 1000)
+    public void notFun()
+    {
+        byte[][] init = new byte[][]{           {0, 2, 0, 0, 0, 0, 0, 0, 0},
+                                                {0, 0, 0, 6, 0, 0, 0, 0, 3},
+                                                {0, 7, 4, 0, 8, 0, 0, 0, 0},
+                                                {0, 0, 0, 0, 0, 3, 0, 0, 2},
+                                                {0, 8, 0, 0, 4, 0, 0, 1, 0},
+                                                {6, 0, 0, 5, 0, 0, 0, 0, 0},
+                                                {0, 0, 0, 0, 1, 0, 7, 8, 0},
+                                                {5, 0, 0, 0, 0, 9, 0, 0, 0},
+                                                {0, 0, 0, 0, 0, 0, 0, 4, 0}};
+
+
+
+        Board b = new Board(init);
+
+        while(!b.isSolved())
+        {
+            final Board.Cell next = b.findNextCandidate();
+
+            final byte[] candidates = next.getCandidateValues();
+
+            b = next.setValue(candidates[0]);
+        }
+
+        System.out.println(b.toString());
+    }
+
 }
